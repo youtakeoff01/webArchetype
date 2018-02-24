7461deccf9717719b1780a856b50cb568ec0a6e5
Administrator <18616963221@163.com>
2018-02-24 10:07:12 +0800

    update

--- src/main/java/com/companyname/controller/MyController.java
+++ src/main/java/com/companyname/controller/MyController.java
@@ -2,7 +2,9 @@ package com.companyname.controller;
 
 import java.util.Date;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.http.MediaType;
@@ -18,11 +20,13 @@ import com.companyname.entity.User;
 @RequestMapping(value = "/myController/", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class MyController {
 	@RequestMapping(value = "login",method = RequestMethod.POST)
-    public Response Save(@RequestBody @Valid User user,HttpServletResponse response) throws Exception{ // user:视图层传给控制层的表单对象；model：控制层返回给视图层的对象
-        Response result = new Response();
+    public Response Save(@RequestBody @Valid User user,HttpServletRequest request,HttpServletResponse response) throws Exception{ // user:视图层传给控制层的表单对象；model：控制层返回给视图层的对象
+		HttpSession session = request.getSession(true);
+		Response result = new Response();
          if("yixing".equals(user.getName()) && "123456".equals(user.getPwd())) {
         	 user.setCreatTime(new Date());
         	 result.success(user);
+        	 session.setAttribute("user", user);
          }else {
         	 result.failure("用户名或密码错误！");
          }
